88db57b6b2a5de572aae9807c2985b12faddd058
YKT2024 <yokota.e.2022@gmail.com>
2024-11-19 15:51:46 +0800

    変更しました

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
-// ハンバーガーメニューのトグル
-$(document).ready(function() {
-    $('.hamburger-menu').click(function() {
-        $('header nav ul').toggle();
+// // ハンバーガーメニューのトグル
+// $(document).ready(function() {
+//     $('.hamburger-menu').click(function() {
+//         $('header nav ul').toggle();
+//     });
+// });
+
+
+
+$(document).ready(function () {
+    $('.slider').slick({
+        autoplay: true,       //自動再生
+        autoplaySpeed: 2000,  //自動再生のスピード
+        speed: 800,           //スライドするスピード
+        dots: true,           //スライド下のドット
+        arrows: true,         //左右の矢印
+        infinite: true,       //永久にループさせる
     });
 });
